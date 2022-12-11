c9161fc7a075d3af41c8215314f2c69242ae61cf
S M Tusher <85401497+smtusher007@users.noreply.github.com>
2022-12-11 19:36:34 +0600

    Income Service

---
+++ ADV_Tax/BLL/Services/IncomeService.cs
@@ -0,0 +1,75 @@
+﻿using AutoMapper;
+using BLL.DTOs;
+using DAL;
+using DAL.EF.Models;
+using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BLL.Services
+{
+    public class IncomeService
+    {
+        public static List<IncomeDTO> GetIncome()
+        {
+            var data = DataAccessFactory.IncomeDataAccess().Get();
+            var config = new MapperConfiguration(cfg =>
+            {
+                cfg.CreateMap<Income, IncomeDTO>();
+            });
+            var mapper = new Mapper(config);
+            var rt = mapper.Map<List<IncomeDTO>>(data);
+            return rt;
+        }
+        public static IncomeDTO GetIncome(int id)
+        {
+            var data = DataAccessFactory.IncomeDataAccess().Get(id);
+            var config = new MapperConfiguration(cfg =>
+            {
+                cfg.CreateMap<Income, IncomeDTO>();
+            });
+            var mapper = new Mapper(config);
+            var rt = mapper.Map<IncomeDTO>(data);
+            return rt;
+        }
+        public static IncomeDTO AddIncome(IncomeDTO income)
+        {
+            var config = new MapperConfiguration(cfg => {
+                cfg.CreateMap<IncomeDTO, Income>();
+                cfg.CreateMap<Income, IncomeDTO>();
+            });
+            var mapper = new Mapper(config);
+            var dbincome = mapper.Map<Income>(income);
+            var rt = DataAccessFactory.IncomeDataAccess().Add(dbincome);
+            if (rt != null)
+            {
+                return mapper.Map<IncomeDTO>(rt);
+            }
+            return null;
+        }
+        public static IncomeDTO UpdateIncome(IncomeDTO income)
+        {
+            var config = new MapperConfiguration(cfg =>
+            {
+                cfg.CreateMap<IncomeDTO, Income>();
+                cfg.CreateMap<Income, IncomeDTO>();
+            });
+            var mapper = new Mapper(config);
+            var dbincome = mapper.Map<Income>(income);
+            var rt = DataAccessFactory.IncomeDataAccess().Update(dbincome);
+            if(rt != null)
+            {
+                return mapper.Map<IncomeDTO>(rt);
+
+            }
+            return null;
+        }
+        public static bool DeleteIncome(int Id)
+        {
+            return DataAccessFactory.IncomeDataAccess().Delete(Id);
+        }
+    }
+}
--- ADV_Tax/BLL/Services/RoleService.cs
+++ ADV_Tax/BLL/Services/RoleService.cs
@@ -1,4 +1,8 @@
-﻿using System;
+﻿using AutoMapper;
+using BLL.DTOs;
+using DAL;
+using DAL.EF.Models;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -6,7 +10,64 @@ using System.Threading.Tasks;
 
 namespace BLL.Services
 {
-    internal class RoleService
+    public class RoleService
     {
+        public static List<RoleDTO> GetRole()
+        {
+            var data = DataAccessFactory.RoleDataAccess().Get();
+            var config = new MapperConfiguration(cfg =>
+            {
+                cfg.CreateMap<Role, RoleDTO>();
+            });
+            var mapper = new Mapper(config);
+            var rt = mapper.Map<List<RoleDTO>>(data);
+            return rt;
+        }
+        public static RoleDTO GetRole(int id)
+        {
+            var data = DataAccessFactory.RoleDataAccess().Get(id);
+            var config = new MapperConfiguration(cfg =>
+            {
+                cfg.CreateMap<Role, RoleDTO>();
+            });
+            var mapper = new Mapper(config);
+            var rt = mapper.Map<RoleDTO>(data);
+            return rt;
+        }
+        public static RoleDTO AddRole(RoleDTO role)
+        {
+            var config = new MapperConfiguration(cfg => {
+                cfg.CreateMap<RoleDTO, Role>();
+                cfg.CreateMap<Role, RoleDTO>();
+            });
+            var mapper = new Mapper(config);
+            var dbrole = mapper.Map<Role>(role);
+            var rt = DataAccessFactory.RoleDataAccess().Add(dbrole);
+            if (rt != null)
+            {
+                return mapper.Map<RoleDTO>(rt);
+            }
+            return null;
+        }
+        public static RoleDTO UpdateRole(RoleDTO role)
+        {
+            var config = new MapperConfiguration(cfg => {
+                cfg.CreateMap<RoleDTO, Role>();
+                cfg.CreateMap<Role, RoleDTO>();
+            });
+            var mapper = new Mapper(config);
+            var dbrole = mapper.Map<Role>(role);
+            var rt = DataAccessFactory.RoleDataAccess().Update(dbrole);
+            if (rt != null)
+            {
+                return mapper.Map<RoleDTO>(rt);
+            }
+            return null;
+        }
+
+        public static bool DeleteRole(int Id)
+        {
+            return DataAccessFactory.RoleDataAccess().Delete(Id);
+        }
     }
 }
