15e27a2030f1b944eac04de177a492c6af35c562
2018rakibuzzaman <55857298+2018rakibuzzaman@users.noreply.github.com>
2022-12-11 19:30:30 +0600

    DTOs done

---
+++ ADV_Tax/BLL/DTOs/PaymentDTO.cs
@@ -0,0 +1,21 @@
+﻿using DAL.EF.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BLL.DTOs
+{
+    public class PaymentDTO
+    {
+        public int Id { get; set; }
+        public int IncomeId { get; set; }
+        public long PaymentAccount { get; set; }
+        public int TrxId { get; set; }
+        public int Amount { get; set; }
+        public DateTime PaymentDate { get; set; }
+        public DateTime CreatedAt { get; set; }
+        public DateTime UpdatedAt { get; set; }
+    }
+}
