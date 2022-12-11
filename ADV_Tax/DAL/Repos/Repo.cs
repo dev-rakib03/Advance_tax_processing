208a7b30bbd3192b05b66c8281eacd0cb4953ee6
S M Tusher <85401497+smtusher007@users.noreply.github.com>
2022-12-11 15:11:17 +0600

    IRepo & Repo added

---
+++ ADV_Tax/DAL/Repos/Repo.cs
@@ -0,0 +1,18 @@
+﻿using DAL.EF;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DAL.Repos
+{
+    internal class Repo
+    {
+        protected TaxContext db;
+        internal Repo()
+        {
+            db = new TaxContext();
+        }
+    }
+}
